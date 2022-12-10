56dea24d90f7a0ffb8512ab6d3129848fa3646e1
Aytuğ AKIN <75138903+aytuug@users.noreply.github.com>
2022-12-10 16:23:48 +0300

    added docker compose

--- ws/src/main/java/com/aakin/ws/repository/ProductRepository.java
+++ ws/src/main/java/com/aakin/ws/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.aakin.ws.repository;
 
 import com.aakin.ws.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
