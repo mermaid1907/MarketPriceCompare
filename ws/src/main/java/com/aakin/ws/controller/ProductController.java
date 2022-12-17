7f95e6ad51c59eec444f1f744dde47cf92a709b7
Aytuğ AKIN <75138903+aytuug@users.noreply.github.com>
2022-12-18 00:40:39 +0300

    finished frontend using react.js

--- ws/src/main/java/com/aakin/ws/controller/ProductController.java
+++ ws/src/main/java/com/aakin/ws/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "**", allowedHeaders = "**")
+@CrossOrigin
 public class ProductController {
     private final ProductService productService;
 
