e04830db989ae11acf09703fe8f737409c624205
Elif Karagöz <70434376+mermaid1907@users.noreply.github.com>
2022-12-26 09:35:29 +0300

    intersection
    kesişim kümesinin yeni değerler üzeirnden tekrar ehsaplanması

---
+++ data/intersection.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+migros_data = pd.read_csv('migros_data.csv')
+carrefour_data = pd.read_csv('carrefour_data.csv')
+
+
+cheap = list()
+
+
+for product, price in migros_data.values:
+    for product2, price2, _ in carrefour_data.values:
+        if product.replace("_", " ").replace("Kg", "kg").replace("G","g") == product2.replace("_", " ").replace("Kg", "kg").replace("G","g"):
+            if int(price)<int(price2):
+                cheap.append(["Migros",product.replace("_", " "),price])
+            elif int(price2)<int(price):
+                cheap.append(["Carrefour",product.replace("_", " "),price])
+            elif int(price) == int(price2):
+                cheap.append(["Migros",product.replace("_", " "),price])
+            else:
+                pass
+                  
+print(len(cheap))  
+my_df = pd.DataFrame(cheap, columns=['Market_Name','Product_Name','Price'])
+my_df.to_csv('cheapest_1.csv', index=False)         
